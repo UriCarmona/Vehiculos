d92c2c8bd20d57798a4fcb4cb9aa5158746ed791
UriCarmona <carmonauri890@gmail.com>
2025-01-31 17:01:38 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import pandas as pd
 import streamlit as st
 import plotly_express as px
+from st_aggrid import AgGrid, GridOptionsBuilder
 
 df_vehiculos = pd.read_csv ('vehicles_us.csv')
-
 st.header('VEHICLE DATA')
+
+df_group = df_vehiculos.groupby('model')['price'].sum()
+st.dataframe(df_group, use_container_width=True)
+
 hist_button = st.checkbox('build histogram')
 
 
@@ -28,7 +32,5 @@ if disp_button:
     fig = px.scatter(df_vehiculos, x="odometer", y="price") # crear un gráfico de dispersión
 
     st.plotly_chart(fig, use_container_width=True)
-
-
 
 
